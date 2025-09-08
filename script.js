bfce9f2d088e04666b90431d3f4668f3d90e2eda
DFStechBox <dfosterstewart@gmail.com>
2025-09-07 20:23:16 -0700

    2ed commit

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let score = localStorage.getItem("homeScore")
+  
+score = localStorage.getItem("homeScore")
+score = parseInt(score)
+if (!score) {
+  score = 0
+}
+// let scoreSerialized = JSON.stringify(score)
+// let presentHomeScore = localStorage.setItem("homeScore", scoreSerialized)
+// let radix = 10  
 
 
-function homeScoreFunction() {
-  if (score === 0) {
+
+function homeScoreFunction1() {
     score += 1
     document.querySelector(".homeScore").textContent = score;
     localStorage.setItem("homeScore", score)
     console.log(localStorage)
-  }
+  
 
-  scoreSerialized = JSON.stringify(score)
-presentHomeScore = localStorage.setItem("homeScore", scoreSerialized)
+//   scoreSerialized = JSON.stringify(score)
+// presentHomeScore = localStorage.setItem("homeScore", scoreSerialized)
 
- localStorage.getItem("presentHomeScore") 
-    parseInt(presentHomeScore, radix) += 1
-  document.querySelector(".homeScore").textContent = presentHomeScore;
-  localStorage.setItem("homeScore", presentHomeScore)
+//  localStorage.getItem("presentHomeScore") 
+//     parseInt(presentHomeScore, radix) += 1
+//   document.querySelector(".homeScore").textContent = presentHomeScore;
+//   localStorage.setItem("homeScore", presentHomeScore)
 }
