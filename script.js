5ba3bd461b4c5385fa93cb2f04909eda37eb1977
DFStechBox <dfosterstewart@gmail.com>
2025-09-09 10:39:07 -0700

    Added newGameFunction &leadingTeam function

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
   
-// score = localStorage.getItem("homeScore")
-// score = parseInt(score)
-let score = document.querySelector(".homeScore").textContent;
-score = parseInt(score)
-if (!score) {
-  score = 0
+
+let hScore = document.querySelector(".homeScore").textContent;
+hScore = parseInt(hScore)
+if (!hScore) {
+  hScore = 0
+}
+let gScore = document.querySelector(".guestScore").textContent;
+gScore = parseInt(gScore)
+if (!gScore) {
+  gScore = 0
 }
 
-
-
 function homeScoreFunction1() {
-    score += 1
-    document.querySelector(".homeScore").textContent = score;
+    hScore += 1
+    document.querySelector(".homeScore").textContent = hScore;
+    leadingTeam();
 }
 
 function homeScoreFunction2() {
-    score += 2
-    document.querySelector(".homeScore").textContent = score;      
+    hScore += 2
+    document.querySelector(".homeScore").textContent = hScore;
+    leadingTeam();
 }
 
 function homeScoreFunction3() {
-    score += 3
-    document.querySelector(".homeScore").textContent = score;  
+    hScore += 3
+    document.querySelector(".homeScore").textContent = hScore;
+    leadingTeam();
 }
 
 
 function guestScoreFunction1() {
-    score += 1
-    document.querySelector(".guestScore").textContent = score;
+    gScore += 1
+    document.querySelector(".guestScore").textContent = gScore;
+    leadingTeam();
 }
 
 function guestScoreFunction2() {
-    score += 2
-    document.querySelector(".guestScore").textContent = score;      
+    gScore += 2
+    document.querySelector(".guestScore").textContent = gScore;
+    leadingTeam();
 }
 
 function guestScoreFunction3() {
-    score += 3
-    document.querySelector(".guestScore").textContent = score;  
+    gScore += 3
+    document.querySelector(".guestScore").textContent = gScore; 
+    leadingTeam();
+}
+
+function newGameFunction() {
+    hScore = 0
+    gScore = 0
+    document.querySelector(".homeScore").textContent = hScore;  
+    document.querySelector(".guestScore").textContent = gScore;  
+}
+
+function leadingTeam() {
+    if (hScore > gScore) {
+        document.querySelector(".homeScore").style.backgroundColor = "#c9c97d";
+        document.querySelector(".guestScore").style.backgroundColor = "#f97f7e";
+    } else if (gScore > hScore) {
+        document.querySelector(".guestScore").style.backgroundColor = "#c9c97d";
+        document.querySelector(".homeScore").style.backgroundColor = "#f97f7e";
+    } else {
+        document.querySelector(".homeScore").style.backgroundColor = "black";
+        document.querySelector(".guestScore").style.backgroundColor = "black";
+    }
 }
